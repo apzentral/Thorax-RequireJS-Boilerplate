37f46855aec5edb37fc3ca53d37791bbfb7a9af4
Prut Udomwattawee <apzentral@gmail.com>
2013-11-26 20:51:05 -0800

    Fix bug when already define jQuery in the global scope.

--- js/main.js
+++ js/main.js
@@ -1,50 +1,52 @@
 // Main Config
 
 require.config({
-	// The shim config allows us to configure dependencies for
-	// scripts that do not call define() to register a module
-	shim: {
-		'underscore': {
-			exports: '_'
-		},
-		'backbone': {
-			deps: [
-				'underscore',
-				'jquery'
-			],
-			exports: 'Backbone'
-		},
-		'thorax': {
-			deps: [
-				'underscore',
-				'backbone',
-				'jquery',
-				'handlebars'
-			],
-			exports: 'Thorax'
-		}
-	},
-	paths: {
-		jquery: 'lib/jquery/jquery.min',
-		underscore: 'lib/underscore/lodash.min',
-		backbone: 'lib/backbone/backbone',
-		localstorage: 'lib/backbone/localstorage',
-		handlebars: 'lib/handlebars/handlebars.min',
-		thorax: 'lib/thorax/thorax',
-		text: 'lib/require/text'
-	}
+    // The shim config allows us to configure dependencies for
+    // scripts that do not call define() to register a module
+    shim: {
+        'underscore': {
+            exports: '_'
+        },
+        'backbone': {
+            deps: [
+                'underscore',
+                'jquery'
+            ],
+            exports: 'Backbone'
+        },
+        'thorax': {
+            deps: [
+                'underscore',
+                'backbone',
+                'jquery',
+                'handlebars'
+            ],
+            exports: 'Thorax'
+        }
+    },
+    paths: {
+        jquery: 'lib/jquery/jquery.min',
+        underscore: 'lib/underscore/lodash.min',
+        backbone: 'lib/backbone/backbone',
+        localstorage: 'lib/backbone/localstorage',
+        handlebars: 'lib/handlebars/handlebars.min',
+        thorax: 'lib/thorax/thorax',
+        text: 'lib/require/text'
+    }
 });
 
 require([
-	'views/app',
-	'routers/approuter'
-], function( AppView, AppRouter ) {
-	// Initialize routing and start Backbone.history()
-	new AppRouter();
+    'jquery',
+    'underscore',
+    'views/app',
+    'routers/approuter'
+], function($, _, AppView, AppRouter) {
+    // Initialize routing and start Backbone.history()
+    new AppRouter();
 
-	// Initialize the application view
-	var view = new AppView();
-	$('#app-canvas').append(view.el);
+    // Initialize the application view
+    var view = new AppView();
+    $('#app-canvas').append(view.el);
 
-	Backbone.history.start();
+    Backbone.history.start();
 });
